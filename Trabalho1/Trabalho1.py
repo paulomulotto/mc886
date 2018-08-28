8dfbe4882b7183afae111deca59e461948681703
Vitor Aoki <vitor-27@hotmail.com>
2018-08-27 22:09:15 -0300

    Separação de dados do csv

--- Trabalho1/Trabalho1.py
+++ Trabalho1/Trabalho1.py
@@ -3,11 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-'''MAIN'''
-if __name__ == '__main__':
+def main():
 
     '''Le o arquivo csv com os dados dos diamantes'''
     pd_data = pd.read_csv("diamonds-test.csv")
 
+    '''Realiza a separacao das features cujo valor, nao sao numericos'''
+    pd_data_value = pd.get_dummies(pd_data)
+
     '''Transforma o dataframe do pandas para numpay array'''
-    np_data = pd_data.values
+    np_data = pd_data_value.values
+
+'''MAIN'''
+if __name__ == '__main__':
+
+    main()
+
+
+
